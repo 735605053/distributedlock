bab0fb90b7ea351cfb7c324fba39ec2c87c2aedb
enson16855 <enson16855@hotmail.com>
2020-05-15 23:43:42 +0800

    init...

---
+++ distributedlock-spring/src/main/java/com/daoshuai/distributedlockspring/DistributedlockSpringApplication.java
@@ -0,0 +1,13 @@
+package com.daoshuai.distributedlockspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedlockSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedlockSpringApplication.class, args);
+	}
+
+}
---
+++ distributedlock-spring/src/test/java/com/daoshuai/distributedlockspring/DistributedlockSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daoshuai.distributedlockspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedlockSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
